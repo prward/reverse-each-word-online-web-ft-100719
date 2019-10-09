83cb5b246aa3ef922974d03c9a684ba9ebd5862e
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-09 14:11:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
 def reverse_each_word(sentence)
-  original_array = sentence.split(" ")
-  return_array = []|
+   original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
     return_array << string.reverse
   end
   return_array.join(" ")
 end
-    
-    
